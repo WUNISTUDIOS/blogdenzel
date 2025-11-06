13b099f60dec539ba407d0e0d96e6644ea397eb6
Denzel Arthur <cgimac@macbookair.localdomain>
2025-11-06 18:18:03 +0100

    new route

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,20 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: '/blogdenzel',
+  images: {
+  unoptimized: true, // Required for static export
+  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  trailingSlash: true,
 }
 
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
+    options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
 })
 
 export default withMDX(nextConfig)
